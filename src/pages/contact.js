32bb8cb4b4948aac09f254f0dd028821a0eb93fe
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-12 20:56:36 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ function ContactPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Contact"
       />
-      <section className='flex flex-row mx-24'>
+      <section className='flex flex-row mx-24 bg-pink-200 shadow-lg rounded-lg'>
         <form className="flex flex-col w-1/2 h-14 ml-16">
           <p className='mb-8 w-1/2 leading-loose text-3xl font-bold'>
             KONTAKTA OSS!
@@ -58,14 +58,19 @@ function ContactPage() {
             rows="8"
           />
 
-          <button className="w-48 px-4 py-2 text-sm font-bold text-white bg-gray-700 border-b-4 border-gray-800 rounded hover:border-gray-700 hover:bg-gray-600">
+          <button className="w-48 px-4 py-2 text-sm font-bold text-white bg-pink-700 border-b-4 border-pink-700 rounded hover:border-pink-600 hover:bg-pink-600">
             Skicka
           </button>
         </form>
         
-        <div className='w-1/2 h-14'>
-           
-          <h1 className='flex justify-center text-3xl font-bold mb-8 mr-24 leading-loose'>KONTAKT INFORMATION</h1>
+        <div className='w-1/2 h-14'> 
+          <h1 className='flex text-3xl font-bold mb-8 mr-24 leading-loose'>KONTAKT INFORMATION</h1>
+          <h3 className='font-bold'>Address</h3>
+          <p className='font-medium text-gray-600'> Wenner-Gren Center plan 16 <br></br>
+              c/o DNA Labs AB <br></br>
+              Sveavägen 166 <br></br>
+              113 46 Stockholm, Sweden
+          </p>
           
         </div>
       </section>
