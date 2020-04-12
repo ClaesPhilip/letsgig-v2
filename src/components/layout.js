eca0acbe2cd6d094aa69048b25dfb29ca13c0632
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-12 20:56:40 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,9 +12,9 @@ function Layout({ children }) {
         {children}
       </main>
 
-      <footer className="bg-blue-700">
+      <footer className="border border-black">
         <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
-          <p className="text-white">
+          <p className="text-black">
             Created by{` `}
             <a
               className="font-bold no-underline"
@@ -28,7 +28,7 @@ function Layout({ children }) {
 
           <p>
             <a
-              className="font-bold text-white no-underline"
+              className="font-bold text-black no-underline"
               href="https://github.com/taylorbryant/gatsby-starter-tailwind"
               target="_blank"
               rel="noopener noreferrer"
