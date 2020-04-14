3438eae34eee94b5224792a416a714f7f8c86a58
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-14 16:12:58 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import MainPic from "../images/mainPic.jpeg";
 
 function IndexPage() {
   return (
@@ -12,29 +12,12 @@ function IndexPage() {
         title="Home"
       />
 
-      <section className="text-center">
+      <section className="">
         <img
-          alt="Cat and human sitting on a couch"
-          className="block w-1/2 mx-auto mb-8"
-          src={catAndHumanIllustration}
+          alt="picture"
+          className=""
+          src={MainPic}
         />
-
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Hey there! Welcome to your first Gatsby site.
-        </h2>
-
-        <p className="leading-loose">
-          This is a barebones starter for Gatsby styled using{` `}
-          <a
-            className="font-bold text-gray-900 no-underline"
-            href="https://tailwindcss.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwind CSS
-          </a>
-          , a utility-first CSS framework.
-        </p>
       </section>
     </Layout>
   );
