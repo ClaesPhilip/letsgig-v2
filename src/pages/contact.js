e48e8104d10fd8b4e0f8a8dbda43a7319b118c47
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-15 15:20:54 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,8 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+import Bg_image from "../images/bg-Image.jpg";
+
 function ContactPage() {
   return (
     <Layout>
@@ -10,8 +12,10 @@ function ContactPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`]}
         title="Contact"
       />
-      <section className='flex flex-row mx-24'>
-        <form className="flex flex-col w-1/2 h-14 ml-32">
+      <section className='flex flex-col items-center md:flex-row md:w-full pt-16' style={{
+        backgroundImage: `url(${Bg_image})`
+      }} >
+        <form className="md:w-2/3 md:mr-8 xl:w-1/2 lg:ml-16 sm:w-full flex flex-col sm:ml-16 xs:w-64 sm:w-full">
           <p className='mb-8 w-1/2 leading-loose text-3xl font-bold'>
             KONTAKTA OSS!
           </p>
@@ -58,12 +62,12 @@ function ContactPage() {
             rows="8"
           />
 
-          <button className="w-48 px-4 py-2 text-sm font-bold text-white bg-pink-700 border-b-4 border-pink-700 rounded hover:border-pink-600 hover:bg-pink-600">
+          <button className="flex w-48 px-4 py-2 text-sm font-bold text-white bg-pink-700 border-b-4 border-pink-700 rounded hover:border-pink-600 hover:bg-pink-600">
             Skicka
           </button>
         </form>
-        
-        <div className='w-1/2 h-14'> 
+
+        {/* <div className='w-1/2 h-14'> 
           <h1 className='flex text-3xl font-bold mb-8 mr-24 leading-loose'>KONTAKT INFORMATION</h1>
           <h3 className='font-bold'>Address</h3>
           <p className='font-medium text-gray-600'> Wenner-Gren Center plan 16 <br></br>
@@ -72,9 +76,9 @@ function ContactPage() {
               113 46 Stockholm, Sweden
           </p>
           
-        </div>
+        </div> */}
       </section>
-    </Layout>
+    </Layout >
   );
 }
 
