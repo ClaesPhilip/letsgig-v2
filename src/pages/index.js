a24623e36df5c8a4be1a2cf5f1a7078e6d6526d8
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-15 15:20:56 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import MainPic from "../images/mainPic.jpeg";
+// import MainPic from "../images/mainPic.jpeg";
 
 function IndexPage() {
   return (
@@ -12,13 +12,13 @@ function IndexPage() {
         title="Home"
       />
 
-      <section className="">
+      {/* <section className="">
         <img
           alt="picture"
           className=""
           src={MainPic}
         />
-      </section>
+      </section> */}
     </Layout>
   );
 }
