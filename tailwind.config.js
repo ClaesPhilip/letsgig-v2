28883f70de2ab2965f0606ed8b6903ab97796f1a
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-21 14:52:11 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  theme: {},
+  theme: {
+    screens: {
+
+      'mobile': '300px',
+      // => @media (min-width: 300px) { ... } 
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
   variants: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
