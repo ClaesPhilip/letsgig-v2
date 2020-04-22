d7dbd09fd196094bbda3b17be6e7c3968dbb7bb0
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-22 14:31:18 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,9 +23,24 @@ function IndexPage() {
 
             <div className="lg:w-3/4 sm:w-3/5 h-12 "></div>
 
-            <p className='lg:w-1/4 sm:w-2/5 text-pink-700 text-4xl font-semibold items-right flex-no-wrap mobile:invisible md:visible'>Senaste giggen: <br></br> UX/UI Designer</p>
+            <p className='lg:w-1/4 sm:w-2/5 text-pink-700 text-4xl font-semibold items-right flex-no-wrap mobile:invisible lg:visible'>Senaste giggen: <br></br> UX/UI Designer</p>
 
       </div>
+
+    <span className='flex flex-col'>
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+      <span>&nbsp;</span>
+    </span>
+    
+      <div className='w-full pt-24'>
+        <h1 className='text-white'>Hitta Gig</h1>
+          <input className="w-1/5 h-12" type="text" placeholder="Search" />
+            <button className='w-1/12 h-12 text-white bg-pink-600 rounded-lg'>Search</button>
+      </div>
+
       </section>
     <div className='w-full h-40'> 
       <div className='w-1/3'>
