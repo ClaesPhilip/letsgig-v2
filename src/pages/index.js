d5d57c99850fe23e4103f169eb66bf1854462ddc
AdrianaAazami <adriana.aazami@outlook.com>
2020-05-07 12:43:47 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,9 @@ import SEO from "../components/seo";
 
 // IMAGES
 // import Bg_Image from "../images/bg-Image.jpg";
-import AppStore from '../images/appStore.jpg';
-import GooglePlay from '../images/googlePlay.png';
-import Mecenat from '../images/mecenat.png';
-import MainPic from '../images/mainPic.jpeg';
+import AppStore from '../images/applelogo.png';
+import GooglePlay from '../images/android.png';
+import MainPic from '../images/header.svg';
 
 // ICONS
 import { MdExpandMore } from "react-icons/md";
@@ -31,60 +30,49 @@ function IndexPage() {
         backgroundImage: `url(${MainPic})`
       }} >
 
-      <div className='pt-24 flex'>
-            <div className="lg:w-3/4 sm:w-3/5 h-12 "></div>
-            <p className='lg:w-1/4 sm:w-2/5 text-pink-700 text-4xl font-semibold items-right flex-no-wrap mobile:invisible lg:visible'>Senaste giggen: <br></br> <p className='animate-text'>UX/UI Designer</p></p>
-      </div>
+        <div className='pt-24 flex'>
+          <div className="lg:w-3/4 sm:w-3/5 h-12 "></div>
+          <p className='lg:w-1/4 sm:w-2/5 text-pink-700 text-4xl font-semibold items-right flex-no-wrap mobile:invisible lg:visible'>Senaste giggen: <br></br> <p className='animate-text'>UX/UI Designer</p></p>
+        </div>
 
-    <span className='flex flex-col'>
-      <span>&nbsp;</span>
-      <span>&nbsp;</span>
-      <span>&nbsp;</span>
-      <span>&nbsp;</span>
-      <span>&nbsp;</span>
-    </span>
 
-    <h1 className='flex justify-end text-white mb-2 w-1/3 pr-20 text-xl'>Hitta Gig:</h1>
+        {/* <h1 className='flex justify-end text-white mb-2 w-1/3 pr-20 text-xl'>Hitta Gig:</h1> */}
 
-      <div className='flex justify-center w-full h-24'>
+        {/* <div className='flex justify-center w-full h-24'>
           <input className="w-1/5 h-12 rounded-full mr-6 pl-4" type="text" placeholder="Stad" />
-          <input className="w-1/5 h-12 rounded-full mr-6 pl-4" type="text" placeholder="Kategori"/> 
-            <button className='w-1/12 h-12 text-white bg-pink-600 rounded-full '>Search</button>
-      </div>
+          <input className="w-1/5 h-12 rounded-full mr-6 pl-4" type="text" placeholder="Kategori" />
+          <button className='w-1/12 h-12 text-white bg-pink-600 rounded-full '>Search</button>
+        </div> */}
 
       </section>
-    <div className='w-full h-40 flex flex-row pt-4'> 
-      <div className='w-1/3'>
-          <p className='flex justify-center'>Ladda ned appen!</p>
-          
-        <div className='flex flex-row justify-center'>
-          <a href="https://apps.apple.com/se/app/letsgig/id1448023337">
-            <img alt="app store" src={AppStore} width="200" height="100"></img>
-          </a>
-          <a href="https://play.google.com/store/apps/details?id=com.letsgig.prd&hl=sv">
-            <img alt="google play" src={GooglePlay} width="200" height="100"></img>
-          </a>
-        </div>
-      </div>
+      <div className='w-full h-40 flex flex-row pt-4'>
+        <div className='w-1/3'>
 
-      <div className='w-1/3'>
-          <p className='flex justify-center pb-6'>Kom Igång</p>
-          
-        <div className='flex flex-row justify-center border-none'>
-          <button className='w-12 h-12 bg-pink-600 text-3xl text-white rounded-full border-none'><MdExpandMore className='w-full text-4xl'/></button>
-          
-        </div>
-      </div>
 
-      <div className='w-1/3 pt-10'>
-        <div className='flex flex-row justify-center'>
-          <a href="https://mecenat.com/se">
-            <img alt="mecenat" src={Mecenat} width="300" height="100"></img>
-          </a>
+          <div className='flex flex-row justify-left'>
+            <a href="https://apps.apple.com/se/app/letsgig/id1448023337">
+              <img alt="app store" src={AppStore} width="80" height="100"></img>
+            </a>
+            <a href="https://play.google.com/store/apps/details?id=com.letsgig.prd&hl=sv">
+              <img alt="google play" src={GooglePlay} width="80" height="100"></img>
+            </a>
+          </div>
+        </div>
+
+        <div className='w-1/3'>
+
+
+          <div className='flex flex-row justify-center border-none'>
+            <button className='w-12 h-12 text-3xl text-pink-600 border-none'><MdExpandMore className='w-full text-4xl' /></button>
+
+          </div>
+        </div>
+
+        <div className='w-1/3 pt-10'>
+
         </div>
       </div>
-    </div>
-    <FlipCards />
+      <FlipCards />
     </Layout>
   );
 }
