c7a27f3a4e39839d9eea9f675640779332f473f1
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-07 14:56:33 +0200

    Create infoRegister.js

---
+++ src/components/infoRegister.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// import Layout from "../components/layout";
+// import SEO from "../components/seo";
+
+
+
+function InfoRegister() {
+  return (
+   
+      
+    <div>
+    <div className="infoRegister">
+        <div className="infoBlue"></div>
+        <div className="infoPink"></div>
+    </div>
+    </div>
+  );
+}
+
+export default InfoRegister;
