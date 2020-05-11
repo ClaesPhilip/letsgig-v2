3cd48897c7888893e13fb9670c96a51917df2120
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-11 17:17:13 +0200

    Update infoRegister.js

--- src/components/infoRegister.js
+++ src/components/infoRegister.js
@@ -3,7 +3,7 @@ import React from "react";
 // import Layout from "../components/layout";
 // import SEO from "../components/seo";
 
-
+import GirlImage from '../images/person.png';
 
 function InfoRegister() {
   return (
@@ -12,16 +12,19 @@ function InfoRegister() {
     
     <div className="infoRegister">
         <div className="infoBlue">
-          <div className='flex flex-col mt-24 lg:px-24 md:px-12'>
-            <h1 className='flex justify-center text-xl font-medium'>Hej student!</h1>
-            <ul className='flex flex-col h-full'>
-              <li className='lg:text-4xl mobile:text-2xl pb-10'>Du registrerar dig enkelt genom att ladda ned LetsGig appen.</li>
-              <li className='lg:text-4xl mobile:text-2xl pb-10'>Med ett par klick har du skapat en profil som blir ditt digitala CV.</li>
-              <li className='lg:text-4xl mobile:text-2xl pb-10'>Du bestämmer själv hur mycket du vill jobba och inom vilka områden.</li>
-              <li className='lg:text-4xl mobile:text-2xl pb-10'>Chatta direkt med arbetsgivare i appen.</li>
-              <li className='lg:text-4xl mobile:text-2xl pb-10'>Tjänsten är givetvis gratis och du binder inte upp dig.</li>
+          <div className='flex flex-col mt-12 lg:px-24 md:px-12 '>
+            <h1 className='flex justify-center mb-10 text-4xl font-medium'>Hej student!</h1>
+            <ul className='flex flex-col h-full relative'>
+              <li className='lg:text-3xl mobile:text-2xl pb-10'>Du registrerar dig enkelt genom att ladda ned LetsGig appen.</li>
+              <li className='lg:text-3xl mobile:text-2xl pb-10'>Med ett par klick har du skapat en profil som blir ditt digitala CV.</li>
+              <li className='lg:text-3xl mobile:text-2xl pb-10'>Du bestämmer själv hur mycket du vill jobba och inom vilka områden.</li>
+              <li className='lg:text-3xl mobile:text-2xl pb-10'>Chatta direkt med arbetsgivare i appen.</li>
+              <li className='lg:text-3xl mobile:text-2xl'>Tjänsten är givetvis gratis och du binder inte upp dig.</li>
             </ul>
+            <img src={GirlImage} className='flex lg:w-24 mobile:w-16 relative fixed self-end'></img>
+            <div className='flex justify-center mt-2'>
             <a href='/' className='regButton'>Registrera dig</a>
+            </div>
           </div>
         </div>
 
