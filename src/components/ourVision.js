cb7e53a880f00215822e0d935d708447cbc457ee
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-12 20:54:46 +0200

    Update ourVision.js

--- src/components/ourVision.js
+++ src/components/ourVision.js
@@ -20,23 +20,26 @@ function OurVision() {
       
       <h1 className='flex justify-center text-4xl mt-12'>Vår Vision</h1>
         
-        <div className="flex mb-4 mt-24 mx-24 mobile:flex-col mobile:items-center justify-center lg:flex-row content-center">
+        <div className="flex mb-4 mt-10 mx-24 mobile:flex-col mobile:items-center justify-center lg:flex-row content-center justify-evenly">
           
-          <div className=" h-12 px-24 order-1">
-            <img className='' src={Graduation} width='300px' height='200px'></img>
-          </div>
+          <img className='' src={Graduation} width='300px' height='200px'></img>
           
-          <div className=" h-12 px-24 order-2">
-          <img className='' src={VisionPhone} width='300px' height='200px'></img>
-          </div>
+          <img className='mt-10' src={VisionPhone} width='300px' height='200px'></img>
           
-          <div className=" h-12 px-24 order-3">
           <img className='' src={Balloon} width='300px' height='200px'></img>
-          </div>
+          
         </div>
+
+        <div className='flex justify-evenly items-center ml-10'>
+        <h1 className='justify-center text-3xl'>Studenter först</h1>
+        <h1 className='justify-center text-3xl '>Ansök enkelt</h1>
+        <h1 className='justify-center text-3xl '>Letsgig är din möjlighet</h1>
+        </div>
+
     </div>
   
   );
 }
 
 export default OurVision;
+
