ce8414f54d5eb56ae66dbf878f549c2c16ec80f4
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-13 00:17:05 +0200

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Card = ({property}) => {
-    const {index, picture, city, address, bedrooms, bathrooms, carSpaces} = property;
+    const {index, picture, city, title, bedrooms, bathrooms, carSpaces} = property;
     return (
         <div id={`card-${index}`} className="card">
             <img src={picture} alt={city} />
             <div className="details">
-                <span className="index">{index+1}</span>
+                {/* <span className="index">{index+1}</span> */}
                 <p className="location">
-                    {city}<br />
-                    {address}
+                    {title}<br />
+                    {city}
                 </p>
                 <ul className="features">
                     <li className="icon-bed">{bedrooms} <span>bedrooms</span></li>
