e2eb90c6c1604e0c85952e957c89ccc123ccaaac
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-12 14:23:59 +0200

    Update infoRegister.js

--- src/components/infoRegister.js
+++ src/components/infoRegister.js
@@ -16,7 +16,7 @@ function InfoRegister() {
     
     <div className="infoRegister">
         <div className="infoBlue">
-          <div className='flex flex-col mt-12 lg:px-24 md:px-12 '>
+          <div className='flex flex-col mt-12 lg:px-24 md:px-12 mobile:px-6 '>
             <h1 className='flex justify-center mb-10 text-4xl font-medium'>Hej student!</h1>
             <ul className='flex flex-col h-full relative'>
               <li className='lg:text-3xl mobile:text-2xl pb-10'>Du registrerar dig enkelt genom att ladda ned LetsGig appen.</li>
@@ -26,14 +26,16 @@ function InfoRegister() {
               <li className='lg:text-3xl mobile:text-2xl'>Tjänsten är givetvis gratis och du binder inte upp dig.</li>
             </ul>
             <img src={GirlImage} className='flex lg:w-24 mobile:w-16 relative fixed self-end'></img>
-            <div className='flex justify-center mt-2'>
+            <div className='flex justify-center '>
             <a href='/' className='regButton'>Registrera dig</a>
             </div>
           </div>
         </div>
 
 
-        <div className="infoPink"></div>
+        <div className="infoPink">
+          
+        </div>
     </div>
     
   );
