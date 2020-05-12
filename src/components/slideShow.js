424542f6651cc2945ecfca7c97b36f7802f0eb2a
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-12 23:52:06 +0200

    Create slideShow.js

---
+++ src/components/slideShow.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import '../css/style.css';
+import Card from '../components/Card';
+import data from '../data/data';
+// import logo from '../images/assets/logo.svg';
+// import logo from '../assets/logo.svg';
+
+// class component
+class SlideShow extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      properties: data.properties,
+      property: data.properties[0]
+    }
+  }
+
+  nextProperty = () => {
+    const newIndex = this.state.property.index+1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
+
+  prevProperty = () => {
+    const newIndex = this.state.property.index-1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
+
+  render() {
+    const {properties, property} = this.state;
+    return (
+      <div className="App">
+
+        <button 
+          onClick={() => this.prevProperty()} 
+          disabled={property.index === 0}
+        >Prev</button>
+        <button 
+          onClick={() => this.nextProperty()} 
+          disabled={property.index === data.properties.length-1}
+        >Next</button>
+        
+
+        <div className="page">
+            <section className='section'>
+                {/* <img src={logo} className="App-logo" alt="logo" />
+                <h1>Image slideshow React tutorial.</h1> */}
+            </section>
+
+            <div className="col">
+              <div className={`cards-slider active-slide-${property.index}`}>
+                <div className="cards-slider-wrapper" style={{
+                  'transform': `translateX(-${property.index*(100/properties.length)}%)`
+                }}>
+                  {
+                    properties.map(property => <Card key={property._id} property={property} />)
+                  }
+                </div>
+              </div>
+            </div>
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SlideShow;
