226db2f5150b870bbfa9af056721ea67869b6b2a
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-23 15:18:30 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,18 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 // IMAGES
-// import Bg_Image from "../images/bg-Image.jpg";
+
 import AppStore from '../images/apple.svg';
 import GooglePlay from '../images/android.svg';
-import MainPic from '../images/header.svg';
+
 
 // ICONS
 import { MdExpandMore } from "react-icons/md";
-// import { BsSearch } from "react-icons/bs";
+
 
 // Components
 import FlipCards from "../components/flipCards";
 import InfoRegister from "../components/infoRegister";
-// import OurVision from '../components/ourVision';
 import SlideShow from '../components/slideShow';
 import Footer from '../components/footer';
 
@@ -30,35 +29,33 @@ function IndexPage() {
         title="Home"
       />
 
-      <section className="landingSection" style={{
-        backgroundImage: `url(${MainPic})`, 
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundColor: 'white',
-        width: '100%',
-        height: 'auto',
-      }} >
+      <section className="bg-contain rutan">
 
         <div className='pt-8 flex'>
           <div className="lg:w-3/4 sm:w-3/5 h-12 "></div>
-          <p className='lg:w-1/4 sm:w-2/5 text-pink-700 text-4xl font-semibold items-right flex-no-wrap mobile:invisible lg:visible'>Senaste giggen: <br></br> <p className='animate-text'>UX/UI Designer</p></p>
+          <p className='lg:w-1/4 sm:w-2/5 text-pink-700 text-4xl font-semibold items-right flex-no-wrap invisible lg:visible'>Senaste giggen: <br></br> <p className='animate-text'>UX/UI Designer</p></p>
         </div>
 
 
-        {/* <h1 className='flex justify-end text-white mb-2 w-1/3 pr-20 text-xl'>Hitta Gig:</h1> */}
-
-        {/* <div className='flex justify-center w-full h-24'>
-          <input className="w-1/5 h-12 rounded-full mr-6 pl-4" type="text" placeholder="Stad" />
-          <input className="w-1/5 h-12 rounded-full mr-6 pl-4" type="text" placeholder="Kategori" />
-          <button className='w-1/12 h-12 text-white bg-pink-600 rounded-full '>Search</button>
-        </div> */}
+        
+        
+        
+        
+        {/* <h1 className='flex justify-end text-white mb-2 w-1/3 pr-20 text-xl'>Hitta Gig:</h1> */} 
 
       </section>
+
+      <div className='container mx-auto mt-6 flex flex-col sm:flex-row justify-center items-center'>
+          <input className="md:w-1/5 h-12 rounded-full md:mr-6 pl-4 border" type="text" placeholder="Stad" />
+          <input className="md:w-1/5 h-12 rounded-full mt-2 md:mr-6 pl-4 border" type="text" placeholder="Kategori" />
+          <a href='/availebleJobs' className='md:w-1/12 w-1/5 h-12 mt-2 text-white bg-pink-600 rounded-full text-center '>Search</a>
+        </div>
+
         <div className='w-full h-40 flex flex-row pt-4 bg-white'>
           <div className='w-1/3'>
 
 
-            <div className='flex flex-row justify-left'>
+            <div className='flex flex-row justify-left invisible md:visible'>
               <a href="https://apps.apple.com/se/app/letsgig/id1448023337">
                 <img alt="app store" src={AppStore} width="80" height="100"></img>
               </a>
@@ -82,7 +79,6 @@ function IndexPage() {
         </div>
       </div>
       <InfoRegister />
-      {/* <OurVision /> */}
       <FlipCards />
       <SlideShow />
       <Footer />
