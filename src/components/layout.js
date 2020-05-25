98e36e8bb156a733e572aa15c8bb39edfe22fe13
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-05-25 19:15:52 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,34 +12,6 @@ function Layout({ children }) {
         {children}
       </main>
 
-
-
-      {/* <footer className="border border-black h-screen">
-        <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
-          <p className="text-black">
-            Created by{` `}
-            <a
-              className="font-bold no-underline"
-              href="https://bryant.io"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Letsgig
-            </a>
-          </p>
-
-          <p>
-            <a
-              className="font-bold text-black no-underline"
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </p>
-        </nav>
-      </footer> */}
     </div>
   );
 }
