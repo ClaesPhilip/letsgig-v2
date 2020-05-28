64d80e72e8ac97e39a8fb4943ac7f50fb6148ef4
AdrianaAazami <adriana.aazami@outlook.com>
2020-05-28 16:20:24 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,8 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 // IMAGES
-
-import AppStore from '../images/apple.svg';
-import GooglePlay from '../images/android.svg';
+import AppStore from '../images/app-store.png';
+import GooglePlay from '../images/google-play.png';
 
 
 // ICONS
@@ -38,33 +37,33 @@ function IndexPage() {
         </div>
 
 
-        
-        
-        
-        
-        {/* <h1 className='flex justify-end text-white mb-2 w-1/3 pr-20 text-xl'>Hitta Gig:</h1> */} 
+
+
+
+
+        {/* <h1 className='flex justify-end text-white mb-2 w-1/3 pr-20 text-xl'>Hitta Gig:</h1> */}
 
       </section>
 
       <div className='container mx-auto mt-6 flex flex-col sm:flex-row justify-center items-center'>
-          <input className="md:w-1/5 h-12 rounded-full mt-2 md:mr-6 pl-4 border outline-none" type="text" placeholder="Stad" />
-          <input className="md:w-1/5 h-12 rounded-full mt-2 md:mr-6 pl-4 border outline-none" type="text" placeholder="Kategori" />
-          <a href='/availableJobs' className='flex md:w-1/12 w-1/5 h-12 mt-2 text-white bg-pink-600 rounded-full text-center items-center justify-center outline-none'>Search</a>
-        </div>
+        <input className="md:w-1/5 h-12 rounded-full mt-2 md:mr-6 pl-4 border outline-none" type="text" placeholder="Stad" />
+        <input className="md:w-1/5 h-12 rounded-full mt-2 md:mr-6 pl-4 border outline-none" type="text" placeholder="Kategori" />
+        <a href='/availableJobs' className='flex md:w-1/12 w-1/5 h-12 mt-2 text-white bg-pink-600 rounded-full text-center items-center justify-center outline-none'>Search</a>
+      </div>
 
-        <div className='w-full h-40 flex flex-row pt-4 bg-white'>
-          <div className='w-1/3'>
+      <div className='w-full h-40 flex flex-row pt-4 bg-white'>
+        <div className='w-1/3'>
 
 
-            <div className='flex items-center justify-left lg:ml-20 md:ml-8 invisible md:visible'>
-              <a href="https://apps.apple.com/se/app/letsgig/id1448023337">
-                <img alt="app store" src={AppStore} width="72" height="100"></img>
-              </a>
-              <a href="https://play.google.com/store/apps/details?id=com.letsgig.prd&hl=sv">
-                <img alt="google play" src={GooglePlay} width="80" height="100"></img>
-              </a>
-            </div>
+          <div className='flex items-center justify-left lg:ml-20 md:ml-8 invisible md:visible'>
+            <a href="https://apps.apple.com/se/app/letsgig/id1448023337">
+              <img alt="app store" src={AppStore} width="130" height="130"></img>
+            </a>
+            <a href="https://play.google.com/store/apps/details?id=com.letsgig.prd&hl=sv">
+              <img alt="google play" src={GooglePlay} width="130" height="130"></img>
+            </a>
           </div>
+        </div>
 
         <div className='w-1/3'>
 
